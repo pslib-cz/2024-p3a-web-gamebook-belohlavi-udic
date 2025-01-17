d7e80074b98394f155300124b800e33ff2971511
David Udič <david.udic.022@pslib.cz>
2025-01-17 09:03:29 +0100

    clear

--- Gamebook.Server/DTOs/ConnectionDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gamebook.Server.DTOs
-{
-    public class ConnectionDTO
-    {
-        public int ID { get; set; }
-        public int RoomID2 { get; set; } // Only include the related room ID
-        public string ConnectionType { get; set; }
-    }
-}
--- Gamebook.Server/Models/Location.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gamebook.Server.Models
-{
-    public class Location
-    {
-        public int Id { get; set; } // Změna na Id
-        public required string Title { get; set; }
-        public required string Description { get; set; }
-    }
-}
